3f1efd0adf2e2f49d2e0c5df48e04138a185db46
vinodkumarv8197 <vinodkumarv8197@gmail.com>
2019-05-30 21:42:45 +0530

    new project Pig Game1

--- PigGame/app.js
+++ PigGame/app.js
@@ -8,3 +8,43 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer=0;
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+    var dice = Math.floor(Math.random() * 6 + 1);
+    document.querySelector('#current-' + activePlayer).textContent = dice;
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    document.querySelector('#current-' + activePlayer).textContent
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    if(dice !==1){
+
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    }else{
+
+        roundScore = 0;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        document.querySelector('.player-' + activePlayer + '-panel active').classList.add('active');
+
+    }
+    
+
+});
